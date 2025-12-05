26bee0e89012341bfdf310a1b7d1d7ecb65c2b33
ponravisankar <ponravisankar2005@gmail.com>
2025-12-05 22:02:16 +0530

    adding a file

---
+++ add.py
@@ -0,0 +1,17 @@
+a=10;
+b=20;
+print ("Adding two number");
+c=a+b;
+print (c)
+d=a-b;
+print ("Subraction of two numbers");
+print (d)
+e=a*b;
+print("Multiplication of two numbers");
+print (e)
+f=a/b;
+print ("Division of two numbers")
+print (f)
+g=a/100;
+print("Percentage of A")
+
